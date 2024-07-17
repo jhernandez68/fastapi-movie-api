b613908c5c1883a43b88c88deda30765c55f6730
jhernandez68 <jhoanstiven123xd@gmail.com>
2024-07-16 19:23:13 -0500

    ADD: get movies

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
-from typing import Union
 from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 app.title = 'App with FastAPI'
 app.version = '0.0.1'
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
 
 @app.get('/', tags=['Hola mundo HTMLResponse'])
 def message():
@@ -14,6 +25,11 @@ def message():
 def helloWorld():
     return 'Hola Mundo (String)'
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/movies', tags=['Movies'])
+def getMovies():
+    return movies
+
+
+@app.get("/movies/{id}")
+def read_item(id: int):
+    return {"movie_id": id}
